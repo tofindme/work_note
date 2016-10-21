11cab4305db27ac32bb428138cfe2ce79027b1be
YiBin <849397833@qq.com>
2016-10-21 14:23:58 +0800

    update

--- auth/gm/title.php
+++
@@ -1,6 +0,0 @@
-<html>
-<head>
-<title>fgame gm</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-</html>
--- auth/utils/logger_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require("logger.php");
-
-$logger = Logger::getInstance("/root/yibin.log");
-
-
-$logger->writeFile("yibin");
